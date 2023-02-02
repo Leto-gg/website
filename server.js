1a4712450e2ef183c29b7dc5b1b2ceb0957aaab7
Your Name <logan.lentz1@gmail.com>
2023-02-02 05:33:08 -0600

    added dotenv with npm(npm install dotenv)

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express');
+const dotenv = require('dotenv').config();
 const app = express();
-require('dotenv').config();
 
-var webAuth = new auth0.WebAuth({
-    domain: process.env.domain,
-    clientID: process.env.clientID
-  });
-  
+
 app.use(express.static(__dirname + '/public'));
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
+
+
+var webAuth = new auth0.WebAuth({
+    domain: process.env.domain,
+    clientID: process.env.clientID
+  });
