b761a1865947a95da02a05707e38b74dc0986b95
Logan Lentz <logan.lentz1@gmail.com>
2023-02-11 22:16:01 -0600

    new gateway search page
    Updated this repo with the production repo

--- server.js
+++ server.js
@@ -1,18 +1,30 @@
 const express = require('express');
-const auth0 = require('auth0-js');
+/* const auth0 = require('auth0-js'); */
 const app = express();
-const dotenv = require('dotenv').config();
+require('dotenv').config();
+/* require('./lib/your-compiled-code'); */
 
 
 
 
-app.use(express.static(__dirname + '/public'));
+app.use(
+  /*  auth({
+ 
+ 
+     issuerBaseURL: process.env.ISSUER_BASE_URL,
+     baseURL: process.env.BASE_URL,
+     clientID: process.env.CLIENT_ID,
+     secret: process.env.SECRET,
+   }),
+ */
+
+  express.static(__dirname + '/public'));
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
 
-
+/*
 if (!process.env.AUTH0_DOMAIN || !process.env.AUTH0_CLIENT_ID) {
   throw new Error('Make sure to set AUTH0_DOMAIN and AUTH0_CLIENT_ID in your .env file');
 }
@@ -21,3 +33,4 @@ var webAuth = new auth0.WebAuth({
   domain: process.env.AUTH0_DOMAIN,
   clientID: process.env.AUTH0_CLIENT_ID
 });
+*/
